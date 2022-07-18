a5c6c85365c727bb8d104a276b2380640d5f7052
jargalbat <jargalbat@gmail.com>
2022-07-18 21:14:50 +0800

    using vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,13 +1,30 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// *** Using Vue ***
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+        }
+    }
+}).mount('#app');
 
-function addGoal() {
-    const enteredValue = inputEl.value;
-    const listItemEl = document.createElement('li');
-    listItemEl.textContent = enteredValue;
-    listEl.append(listItemEl);
 
-}
+// *** Using JavaScript ***
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', addGoal);
+// function addGoal() {
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li');
+//     listItemEl.textContent = enteredValue;
+//     listEl.append(listItemEl);
+
+// }
+
+// buttonEl.addEventListener('click', addGoal);
