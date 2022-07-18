d06857db2999bce3fa3a1424b980e73a21f3b901
jargalbat <jargalbat@gmail.com>
2022-07-18 23:08:01 +0800

    event method

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,9 +4,23 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add() {
+      this.counter++;
+    },
+    reduce() {
+      this.counter = this.counter - 1;
+    },
+    add5(num) {
+      this.counter+=num;
+    },
+    reduce5(num) {
+      this.counter = this.counter - num;
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
 
 // Using JavaScript
 // document.getElementById
