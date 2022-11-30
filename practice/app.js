0e249c31d13c5a14292cf6468734c82038ae2bc2
jargalbat <jargalbat@mplus.mn>
2022-11-30 09:33:54 +0800

    memorized

--- practice/app.js
+++ practice/app.js
@@ -3,12 +3,10 @@ Vue.createApp({
     return {
       goals: [],
       enteredValue: "",
-      items: [{ message: "Foo" }, { message: "Bar" }],
     };
   },
   methods: {
     addGoal() {
-      console.log("asdf");
       this.goals.push(this.enteredValue);
       this.enteredValue = "";
     },
--- practice/vanilla.js
+++ practice/vanilla.js
@@ -3,7 +3,6 @@ const buttonEl = document.querySelector("button");
 const ulEl = document.querySelector("ul");
 
 buttonEl.addEventListener("click", () => {
-  console.log(inputEl.value);
   const liEl = document.createElement("li");
   liEl.textContent = inputEl.value;
   ulEl.appendChild(liEl);
